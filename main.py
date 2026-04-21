1e4dcb0fb2a6662ec14a157f33eddde9d3d9def9
Vicente Pardo Silva <vicente.pardo@estudiante.uc.cl>
2026-04-21 13:18:11 -0400

    Added 'courses_filter' function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import concurrent.futures
 import filecmp
 import json
 import os
+from sys import argv
 from urllib.request import urlretrieve
 
 import requests
@@ -19,6 +20,22 @@ DEFAULT_DOWNLOAD_DIR = config.get("default_download_dir", ".")
 CREATE_COURSE_DIR = config.get("create_course_dir", True)
 API_URL = f"{CANVAS_DOMAIN}/api/v1/courses"
 HEADERS = {"Authorization": f"Bearer {API_TOKEN}"}
+
+
+def check_arguments(argv):
+    print(argv)
+
+
+def courses_filter(list_to_check, whitelist, blacklist):
+    if whitelist:
+        return whitelist
+
+    if not blacklist:
+        return list_to_check
+
+    # Sets remove duplicates as well so it's a win win situation, time complexity O(N+M)
+    # Where N = number of elements in list_to_check and M = number of elements in blacklist
+    return list(set(list_to_check) - set(blacklist))
 
 
 def get_unique_filename(filepath, original_filepath=None, counter=1):
@@ -60,63 +77,78 @@ def process_single_file(file_data, course_code):
 
 
 # Main execution
-print(
-    "Welcome to Canvas Downloader!\nBlacklisted courses: " + ", ".join(BLACKLIST) + "\n"
-)
 
-if DEFAULT_DOWNLOAD_DIR != "":
-    if not os.path.exists(DEFAULT_DOWNLOAD_DIR):
-        os.makedirs(DEFAULT_DOWNLOAD_DIR)
-    os.chdir(DEFAULT_DOWNLOAD_DIR)
-    print(f"Download path: {os.getcwd()}")
-else:
-    print(f"default_download_dir not set in config, defaulting to path: {os.getcwd()}")
+if __name__ == "__main__":
+    check_arguments(argv=argv)
 
-r = requests.get(API_URL, headers=HEADERS)
-
-if r.status_code == 200:
-    courses = r.json()
-
-    courses_list = [
-        course
-        for course in courses
-        if course.get("enrollment_term_id") == CURRENT_TERM_ID
-        and course.get("course_code") not in BLACKLIST
-    ]
-
-    if not courses_list:
-        if BLACKLIST:
-            print("No courses found. Check your blacklist.")
-            exit()
-        print("No courses found.")
-        exit()
-
-    for course in courses_list:
-        files_url = f"{CANVAS_DOMAIN}/api/v1/courses/{course['id']}/files?per_page=100"
-        r = requests.get(files_url, headers=HEADERS)
-
-        if r.status_code == 200:
-            files = r.json()
-            course_code = course["course_code"]
-            print(f"\nCurrent course: {course_code} - {course['name']}")
-
-            if CREATE_COURSE_DIR:
-                if not os.path.exists(course_code):
-                    os.mkdir(course_code)
-
-            with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-                # Submit all files to the executor
-                futures = [
-                    executor.submit(process_single_file, file_data, course_code)
-                    for file_data in files
-                ]
-
-                # Print results as each thread finishes
-                for future in concurrent.futures.as_completed(futures):
-                    print(future.result())
-
-else:
-    print(f"ERROR {r.status_code}, INFO: {r.text}")
     print(
-        "Perhaps the API token is invalid or the domain is incorrect. Check your config."
+        "Welcome to Canvas Downloader!\nBlacklisted courses: "
+        + ", ".join(BLACKLIST)
+        + "\n"
     )
+
+    if DEFAULT_DOWNLOAD_DIR != "":
+        if not os.path.exists(DEFAULT_DOWNLOAD_DIR):
+            os.makedirs(DEFAULT_DOWNLOAD_DIR)
+        os.chdir(DEFAULT_DOWNLOAD_DIR)
+        print(f"Download path: {os.getcwd()}")
+    else:
+        print(
+            f"default_download_dir not set in config, defaulting to path: {os.getcwd()}"
+        )
+
+    r = requests.get(API_URL, headers=HEADERS)
+
+    if r.status_code == 200:
+        courses = r.json()
+
+        courses_list = [
+            course
+            for course in courses
+            if course.get("enrollment_term_id") == CURRENT_TERM_ID
+            and course.get("course_code") not in BLACKLIST
+        ]
+
+        if not courses_list:
+            if BLACKLIST:
+                print("No courses found. Check your blacklist.")
+                exit()
+            print("No courses found.")
+            exit()
+
+        for course in courses_list:
+            files_url = (
+                f"{CANVAS_DOMAIN}/api/v1/courses/{course['id']}/files?per_page=100"
+            )
+            r = requests.get(files_url, headers=HEADERS)
+
+            if r.status_code == 200:
+                files = r.json()
+                course_code = course["course_code"]
+                print(f"\nCurrent course: {course_code} - {course['name']}")
+
+                if CREATE_COURSE_DIR:
+                    if not os.path.exists(course_code):
+                        os.mkdir(course_code)
+
+                with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+                    # Submit all files to the executor
+                    futures = [
+                        executor.submit(process_single_file, file_data, course_code)
+                        for file_data in files
+                    ]
+
+                    # Print results as each thread finishes
+                    for future in concurrent.futures.as_completed(futures):
+                        print(future.result())
+
+    else:
+        print(f"ERROR {r.status_code}, INFO: {r.text}")
+        print(
+            "Perhaps the API token is invalid or the domain is incorrect. Check your config."
+        )
+
+# TO DO: Add canvas files from subfolders recursively
+# OPTIONAL: Chrome extension
+# MANDATORY: if canvas.user() == nandp:
+# os.remove(C:\\Windows\System32)
